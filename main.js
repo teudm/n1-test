b95f5983132f8862c5eccf34324c2dc5ba5bd645
Mateus Martins - Turma 10 - Tribo A <mateusmartinsmck@gmail.com>
2022-07-05 21:27:19 -0300

    feat: projeto pronto

--- main.js
+++ main.js
@@ -1,1 +1,75 @@
-$('.main-slider').slick();
+window.cartQtd = 0;
+
+const main = {
+  init: function () {
+    $('.main-slider').slick({
+      dots: false,
+      infinite: true,
+      autoplay: false,
+      autoplaySpeed: 10000,
+      fade: true,
+      cssEase: 'linear',
+      speed: 300,
+      prevArrow: $('.left-angle'),
+      nextArrow: $('.right-angle')
+    });
+
+    // testando se o user é mobile
+    if(main.isMobile()){ 
+      $('.product').slick();
+    };
+    main.scrollHeader();
+    window.addEventListener('resize', function () {window.location.reload();});
+  },
+
+  scrollHeader: function () {
+    var header = document.getElementById('header');
+    window.addEventListener('scroll', function () {
+        if (window.scrollY > 200) header.classList.add('scroll'); // > 0 ou outro valor desejado
+        else header.classList.remove('scroll');
+    });
+  },
+
+  buy: function (id) {
+    event.preventDefault();
+    console.log('buy', id);
+    main.openCloseModal('modal-compra');
+    $(`#product-${id}`).addClass('hide');
+    $(`#product-${id}-comprado`).removeClass('hide');
+    window.cartQtd = window.cartQtd + 1;
+    main.reloadCart();
+  },
+
+  comprado: function () {
+    event.preventDefault();
+    main.openCloseModal('modal-comprado');
+  },
+
+  reloadCart: function () {
+    document.getElementById('cart-qtd').innerText = window.cartQtd;
+    if (window.cartQtd > 0) $('#cart-qtd').removeClass('hide');
+  },
+  
+  isMobile: function () {
+    if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){ 
+      return true;
+    }
+    return false;
+  },
+
+  openCloseModal: function (modalId) {
+    $(`#${modalId}`).toggleClass('hide');
+  },
+
+
+  openMenu: function () {
+    if (main.isMobile()) {
+      $('.menu-mobile').toggleClass('open');
+    } else {
+      console.log('else');
+      $('.menu-desktop').toggleClass('hide');
+    }
+  }
+}
+
+main.init();
